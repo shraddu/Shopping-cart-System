10476ff8c10a28084c1d81a9c4ef1b5a40042c6f
shrvaidy <shrvaidy@LIN25007660.corp.capgemini.com>
2021-12-02 23:38:20 +0530

    updating eureka and apigateway

--- EurekaServer/src/main/java/com/shopping/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/shopping/EurekaServerApplication.java
@@ -2,9 +2,10 @@ package com.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
